b67ebf95272ad634a1d17689cdbabbae5e3bad21
Janett Salzmann <janett.salzmann@sandstorm.de>
2024-02-23 08:52:13 +0100

    TASK: Add typings for ButtonToTop
    Addresses #133

--- app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/ButtonToTop/ButtonToTop.ts
+++ app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/ButtonToTop/ButtonToTop.ts
@@ -1,57 +1,58 @@
-const BUTTON_TO_TOP_WRAPPER_CLASS = "button-to-top__wrapper";
+import { AlpineComponent } from 'alpinejs'
 
-export default () => ({
+const CSS_CLASSES = {
+    buttonWrapper: '.button-to-top__wrapper',
+    buttonWrapperHidden: 'button-to-top__wrapper--is-hidden'
+}
+
+export type ButtonToTopComponent = {
+    previousScrollPosition: number
+
+    init: () => void
+    isScrollingDown: () => boolean
+    handleScroll: () => void
+}
+
+export default (): AlpineComponent<ButtonToTopComponent> => ({
     previousScrollPosition: 0,
+
     init() {
-        //@ts-ignore
-        const buttonToTop = this.$refs.buttonToTop;
+        const buttonToTop = this.$refs.buttonToTop as HTMLButtonElement
 
-        buttonToTop.addEventListener("click", () => {
-            window.scrollTo({ top: 0, behavior: "smooth" });
-        });
-
-        window.addEventListener("scroll", () => this.handleScroll());
+        buttonToTop.addEventListener('click', () => {
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+        })
     },
-    isScrollingDown() {
-        let goingDown = false;
 
-        let scrollPosition = window.pageYOffset;
+    isScrollingDown() {
+        const scrollPosition = window.scrollY
 
         if (scrollPosition > this.previousScrollPosition) {
-            goingDown = true;
+            this.previousScrollPosition = scrollPosition
+
+            return true
         }
 
-        this.previousScrollPosition = scrollPosition;
+        this.previousScrollPosition = scrollPosition
 
-        return goingDown;
+        return false
     },
-    handleScroll() {
-        //@ts-ignore
-        const buttonToTop = this.$refs.buttonToTop;
 
-        const buttonWrapper = buttonToTop.closest(
-            `.${BUTTON_TO_TOP_WRAPPER_CLASS}`
-        );
+    handleScroll() {
+        const buttonToTop = this.$refs.buttonToTop as HTMLButtonElement
+
+        const buttonWrapper = buttonToTop.closest(CSS_CLASSES.buttonWrapper)
+
+        if(!buttonWrapper) return
 
         // We just want to show the button-to-top when the user indicates to scroll up and when we are not already at the top of the page
         // Otherwise we hide the button
-        if (window.pageYOffset === 0 || this.isScrollingDown()) {
-            if (
-                !buttonWrapper.classList.contains(
-                    `${BUTTON_TO_TOP_WRAPPER_CLASS}--is-hidden`
-                )
-            ) {
-                buttonWrapper.classList.add(
-                    `${BUTTON_TO_TOP_WRAPPER_CLASS}--is-hidden`
-                );
+        if (window.scrollY === 0 || this.isScrollingDown()) {
+            if (!buttonWrapper.classList.contains(CSS_CLASSES.buttonWrapperHidden)) {
+                buttonWrapper.classList.add(CSS_CLASSES.buttonWrapperHidden)
             }
-        } else if (
-            !this.isScrollingDown() &&
-            window.pageYOffset > window.innerHeight
-        ) {
-            buttonWrapper.classList.remove(
-                `${BUTTON_TO_TOP_WRAPPER_CLASS}--is-hidden`
-            );
+        } else if (!this.isScrollingDown() && window.scrollY > window.innerHeight) {
+            buttonWrapper.classList.remove(CSS_CLASSES.buttonWrapperHidden)
         }
     },
-});
+})
--- app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/main.ts
+++ app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/main.ts
@@ -1,4 +1,4 @@
-import Alpine from 'alpinejs'
+import Alpine, { AlpineComponent } from 'alpinejs'
 import collapse from '@alpinejs/collapse'
 import intersect from '@alpinejs/intersect'
 import persist from '@alpinejs/persist'
@@ -7,7 +7,7 @@ import eventList from './Fusion/Presentation/Components/Event/List/EventList'
 import { initMap } from './Fusion/Presentation/Components/Map/Map'
 import slider from './Fusion/Presentation/Components/Slider/Slider'
 import logowall from './Fusion/Presentation/Components/Logowall/Logowall'
-import buttonToTop from './Fusion/Presentation/Components/ButtonToTop/ButtonToTop'
+import ButtonToTop, { ButtonToTopComponent } from './Fusion/Presentation/Components/ButtonToTop/ButtonToTop'
 
 // We decided to use https://alpinejs.dev/ to write js code
 // as it provides a great way to structure and develop js components.
@@ -20,7 +20,6 @@ initMap(Alpine)
 Alpine.data('eventList', eventList)
 Alpine.data('logowall', logowall)
 Alpine.data('slider', slider)
-Alpine.data('buttonToTop', buttonToTop)
-
+Alpine.data('buttonToTop', ButtonToTop as (value: any) => AlpineComponent<ButtonToTopComponent>)
 
 Alpine.start()
