7e23b60c2fdbe30efa73cdd3e5b0f31e0c4a9886
Anne Menzel <anne.menzel@sandstorm.de>
2024-03-13 13:21:36 +0100

    TASK: add query sanitizer for filter

--- app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/Event/List/EventList.ts
+++ app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/Fusion/Presentation/Components/Event/List/EventList.ts
@@ -10,6 +10,7 @@ export type EventListComponent = {
     filterByTagClick: (event: PointerEvent) => void
     deleteFilter: () => void
     setNewUrl: (urlParams: URLSearchParams) => void
+    sanitizeQueryParameter: (text: string) => string
 }
 
 export default (): AlpineComponent<EventListComponent> => ({
@@ -27,7 +28,7 @@ export default (): AlpineComponent<EventListComponent> => ({
     // and the current tags -> sets the new query parameter and reloads the page
     filterByTagClick(event) {
         const element = event.currentTarget as HTMLInputElement
-        const tagName = element.id.toLowerCase()
+        const tagName = this.sanitizeQueryParameter(element.id)
 
         const urlParams = new URLSearchParams(window.location.search)
         const tagsQueryParam = urlParams.get(TAGS_QUERY_PARAM)?.toLowerCase()
@@ -74,5 +75,46 @@ export default (): AlpineComponent<EventListComponent> => ({
 
         // for now we just reload the page, the actual filtering is done by fusion
         location.reload()
+    },
+
+    // the analogue function exists in FilterNodesByReferenceHelper.php -> keep in sync
+    sanitizeQueryParameter(text: string): string {
+        text = text.toLowerCase()
+
+        const regexReplacements: { [key: string]: string } = {
+            'ä': 'ae',
+            'ö': 'oe',
+            'ü': 'ue',
+            'ß': 'ss',
+            '%': '-',
+            'î': 'i',
+            'ç': 'c',
+            '°': 'o',
+            '@': 'at',
+            '[áàâ]': 'a',
+            '[éèê]': 'e',
+            '[óòô]': 'o',
+            '[úùû]': 'u',
+            '[\\(\\)]': '',
+            '[\"<>]': '',
+            '[+,:\'\\s\\/#!?&.*]+': '-',
+            '-+': '-',
+            '(^-)|(-$)': '',
+            '[^a-z0-9._~-]': '_',
+        }
+
+        for (let pattern in regexReplacements) {
+            const regExp = new RegExp(pattern, 'gu')
+            text = text.replace(regExp, regexReplacements[pattern])
+        }
+
+        // remove duplicates
+        const noDuplicates = ['-', '_']
+        noDuplicates.forEach((char) => {
+            const regExp = new RegExp(char + char + '+', 'g')
+            text = text.replace(regExp, char)
+        })
+
+        return text
     },
 })
