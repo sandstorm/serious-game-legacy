159c6a419b1ffbd3b212924b69019b83806492a2
Anne Menzel <anne.menzel@sandstorm.de>
2024-04-11 10:54:52 +0200

    TASK: extract logo bar to component library (wip)

--- app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/JavaScript/main.ts
+++ app/DistributionPackages/MyVendor.AwesomeNeosProject/Resources/Private/JavaScript/main.ts
@@ -6,9 +6,12 @@ import { ExampleStore } from './Types/types'
 import { EXAMPLE_STORE } from './store'
 import ButtonToTop, { ButtonToTopComponent } from '../Fusion/Presentation/Components/ButtonToTop/ButtonToTop'
 import EventList, { EventListComponent } from '../Fusion/Presentation/Components/Event/List/EventList'
-import LogoBar, { LogoBarComponent } from '../Fusion/Presentation/Components/LogoBar/LogoBar'
 import Map, { MapComponent } from '../Fusion/Presentation/Components/Map/Map'
 import Slider, { SliderComponent } from '../Fusion/Presentation/Components/Slider/Slider'
+
+// start: Component Library Imports //
+import LogoBar, { LogoBarComponent } from '../../../../Sandstorm.ComponentLibrary/Resources/Private/Fusion/Presentation/Components/LogoBar/LogoBar'
+// end: Component Library Imports //
 
 // We decided to use https://alpinejs.dev/ to write js code
 // as it provides a great way to structure and develop js components.
@@ -19,9 +22,12 @@ Alpine.plugin(persist)
 // Components
 Alpine.data('buttonToTop', ButtonToTop as (value: any) => AlpineComponent<ButtonToTopComponent>)
 Alpine.data('eventList', EventList as (value: any) => AlpineComponent<EventListComponent>)
-Alpine.data('logoBar', LogoBar as (value: any) => AlpineComponent<LogoBarComponent>)
 Alpine.data('map', Map as (value: any) => AlpineComponent<MapComponent>)
 Alpine.data('slider', Slider as (value: any) => AlpineComponent<SliderComponent>)
+
+// start: Component Library Components //
+Alpine.data('logoBar', LogoBar as (value: any) => AlpineComponent<LogoBarComponent>)
+// end: Component Library Components //
 
 // Stores
 Alpine.store<ExampleStore>(EXAMPLE_STORE, {
