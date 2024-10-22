2f36cb3785dd5ad8146791ecd9f93529960f4ff3
Michael <michael.berhorst@sandstorm-media.de>
2024-10-22 13:01:19 +0200

    TASK: Refactored Slider
    * logobar and slider use abstract slider

--- app/DistributionPackages/Sandstorm.ComponentLibrary/Resources/Private/Fusion/Presentation/Components/Slider/Slider.ts
+++ app/DistributionPackages/Sandstorm.ComponentLibrary/Resources/Private/Fusion/Presentation/Components/Slider/Slider.ts
@@ -1,36 +1,8 @@
 import { AlpineComponent } from 'alpinejs'
 import Swiper from 'swiper/bundle'
-import { Swiper as SwiperType } from 'swiper'
+import basicSlider, {AbstractSliderComponent, CSS_CLASSES} from './AbstractSlider'
 
-const CSS_CLASSES = {
-    // use the generic swiper classes here so the code works for all swipers inheriting this function
-    slider: '.swiper',
-    slide: 'swiper-slide',
-    pagination: '.swiper-pagination',
-    // We need additional specific classnames for the navigation arrows to make sliders work separatly
-    // Otherwise all slider are connect, which means when I navigation in one slider another slider on the same
-    // page will also navigate to the next slide
-    nextEl: '.swiper-button-next.swiper-button-next--basic-slider',
-    prevEl: '.swiper-button-prev.swiper-button-prev--basic-slider',
-}
-
-export type SliderComponent = {
-    inBackend: boolean
-    prevSlideMessage: string
-    nextSlideMessage: string
-
-    _currentPosition: number
-    _swiper: SwiperType | null
-
-    init: () => void
-    getSlideIndex: (element: HTMLElement) => number
-    isSlide: (element: HTMLElement) => boolean
-    isOwnSlide: (element: HTMLElement) => boolean
-    nodeSelected: (event: any) => void
-    nodeRemoved: (event: any) => void
-    nodeCreated: (event: any) => void
-    _initSlider: () => void
-}
+export type SliderComponent = AbstractSliderComponent
 
 export default (
     prevSlideMessage: string,
@@ -38,88 +10,12 @@ export default (
     inBackend: boolean = false
 ): AlpineComponent<SliderComponent> => {
     return {
-        inBackend: inBackend,
-        prevSlideMessage: prevSlideMessage,
-        nextSlideMessage: nextSlideMessage,
-
-        _currentPosition: 1,
-        _swiper: null as SwiperType | null,
-
-        // init is called before alpine.js renders the appropriate component in the DOM.
-        // In this case we create a new Swiper for the referenced Slider (x-ref="slider") in the DOM.
-        init() {
-            this._initSlider()
-
-            if (this.inBackend) {
-                const scope = this
-                // listen to node events to scroll to the right slide in neos backend
-                document.addEventListener(
-                    'Neos.NodeCreated',
-                    function (event) {
-                        scope.nodeCreated(event)
-                    },
-                    false
-                )
-                document.addEventListener(
-                    'Neos.NodeSelected',
-                    function (event) {
-                        scope.nodeSelected(event)
-                    },
-                    false
-                )
-                document.addEventListener(
-                    'Neos.NodeRemoved',
-                    function (event) {
-                        scope.nodeRemoved(event)
-                    },
-                    false
-                )
-            }
-        },
-
-        // Backend Optimizations
-        getSlideIndex(element) {
-            const slides = Array.from(this.$refs.slider.querySelectorAll(`.${CSS_CLASSES.slide}`))
-            return slides.indexOf(element)
-        },
-
-        isSlide(element) {
-            return element.classList.contains(CSS_CLASSES.slide)
-        },
-
-        isOwnSlide(element) {
-            if (!this.isSlide(element)) return false
-            return element.closest(CSS_CLASSES.slider) === this.$refs.slider
-        },
-
-        nodeSelected(event) {
-            if (this._swiper && this.isOwnSlide(event.detail.element)) {
-                const index = this.getSlideIndex(event.detail.element)
-                if (index >= 0) {
-                    this._swiper.update()
-                    this._swiper.slideTo(index)
-                }
-            }
-        },
-
-        nodeRemoved(event) {
-            if (this._swiper && this.isSlide(event.detail.element)) {
-                // we update all sliders in the page as the parent is null
-                // and we cannot check if it is our own slide
-                this._swiper.update()
-            }
-        },
-
-        nodeCreated(event) {
-            if (this._swiper && this.isOwnSlide(event.detail.element)) {
-                this._swiper.update()
-            }
-        },
+        // We are not rendering the arrows here, so we don't need the a11y messages for prev and next slide
+        ...basicSlider(prevSlideMessage, nextSlideMessage, inBackend),
 
         _initSlider() {
             const swiperRef = this.$refs.slider
-
-            const amountOfSlides = swiperRef.querySelectorAll(`.${CSS_CLASSES.slide}`).length
+            const amountOfSlides = swiperRef.querySelectorAll(CSS_CLASSES.slide).length
 
             if (amountOfSlides === 0) {
                 return
