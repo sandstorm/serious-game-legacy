ddf77011815d9053086dc89aabbe9a34a4d1f610
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2024-12-30 20:10:08 +0100

    make DB seeder idempotent

--- app/database/seeders/DatabaseSeeder.php
+++ app/database/seeders/DatabaseSeeder.php
@@ -15,9 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        if (!User::where('email', 'test@example.com')->exists()) {
+            User::factory()->create([
+                'name' => 'Test User',
+                'email' => 'test@example.com',
+            ]);
+        }
     }
 }
