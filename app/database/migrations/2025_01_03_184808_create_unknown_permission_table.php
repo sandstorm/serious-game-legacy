1a960f01915f59381693cc902a9003a81a59a935
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2025-01-09 14:17:41 +0100

    !!!upmerge!!! fix unknown permission table

--- app/database/migrations/2025_01_03_184808_create_unknown_permission_table.php
+++ app/database/migrations/2025_01_03_184808_create_unknown_permission_table.php
@@ -27,6 +27,6 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('unknown_permission');
+        Schema::dropIfExists('unknown_permissions');
     }
 };
