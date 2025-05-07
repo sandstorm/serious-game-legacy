043d2d6ae1dc4e2a42f1ff377d188b5884724999
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2025-05-07 11:26:46 +0200

    TASK: remove some plugins

--- app/app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
+++ app/app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
@@ -5,14 +5,11 @@ declare(strict_types=1);
 namespace App\Filament\Admin\Resources\UserResource\Pages;
 
 use App\Filament\Admin\Resources\UserResource;
-use Archilex\AdvancedTables\AdvancedTables;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 
 class ListUsers extends ListRecords
 {
-    use AdvancedTables;
-
     protected static string $resource = UserResource::class;
 
     protected function getHeaderActions(): array
