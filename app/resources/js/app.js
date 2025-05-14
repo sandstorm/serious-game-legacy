c1dfc3cd33194581afd4ee5e9e18afa71af4821b
Sebastian Kurfuerst <sebastian.kurfuerst@sandstorm.de>
2025-05-14 11:12:36 +0200

    FEATURE: Plain CSS Design System

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -1,1 +1,4 @@
 import './bootstrap';
+
+import '../css/root.css';
+import.meta.glob('../views/**/*.css', { eager: true });
--- app/resources/views/components/form/submit.blade.php
+++ app/resources/views/components/form/submit.blade.php
@@ -1,3 +1,3 @@
-<button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+<button type="submit" class="form__button isPrimary">
     {{ $slot }}
 </button>
