049db056d57b3ebcc1e7d23fee672c8c888d38df
Michael <michael.berhorst@sandstorm-media.de>
2025-05-22 16:18:30 +0200

    TASK: Attach activate and skip card handler
    Addresses: #49

--- app/app/Livewire/Traits/CardTrait.php
+++ app/app/Livewire/Traits/CardTrait.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace App\Livewire\Traits;
 
+use Domain\CoreGameLogic\Dto\ValueObject\CardId;
+use Domain\CoreGameLogic\Dto\ValueObject\PileId;
+use Domain\CoreGameLogic\Feature\Spielzug\Command\ActivateCard;
+use Domain\CoreGameLogic\Feature\Spielzug\Command\SkipCard;
+
 trait CardTrait
 {
     public ?string $showCardActionsForCard = null;
 
+    /**
+     * @param string $cardId
+     * @return void
+     */
     public function showCardActions(string $cardId): void
     {
         if ($this->showCardActionsForCard === $cardId) {
@@ -17,23 +26,43 @@ trait CardTrait
         }
     }
 
+    /**
+     * @param string $cardId
+     * @return bool
+     */
     public function cardActionsVisible(string $cardId): bool
     {
-        // todo check if requirements are met
         return $this->showCardActionsForCard === $cardId;
     }
 
-    public function activateCard(string $cardId): void
+    /**
+     * @param string $cardId
+     * @param string $pileId
+     * @return void
+     */
+    public function activateCard(string $cardId, string $pileId): void
     {
-        // TODO implement this
-        // $this->gameStream->playCard($cardId);
+        // TODO check if requirements are met
+        $this->coreGameLogic->handle($this->gameId, ActivateCard::create(
+            $this->myself,
+            CardId::fromString($cardId),
+            PileId::fromString($pileId)
+        ));
     }
 
-    public function skipCard(string $cardId): void
+    /**
+     * @param string $cardId
+     * @param string $pileId
+     * @return void
+     */
+    public function skipCard(string $cardId, string $pileId): void
     {
-        // only one card can skipped, the next needs to be played if possible
-        // TODO implement this
-        // $this->gameStream->skipCard($cardId);
+        // TODO check if requirements are met
+        $this->coreGameLogic->handle($this->gameId, new SkipCard(
+            $this->myself,
+            CardId::fromString($cardId),
+            PileId::fromString($pileId)
+        ));
     }
 
 }
--- app/resources/views/components/gameboard/card-pile.blade.php
+++ app/resources/views/components/gameboard/card-pile.blade.php
@@ -2,39 +2,39 @@
     <h3>{{ $title }}</h3>
     <ul class="card-pile__cards">
         @if ($card)
-        <li class="card-pile__card @if ($this->cardActionsVisible($card->id->value)) card-pile__card--show-actions @endif" wire:click="showCardActions('{{$card->id->value}}')">
-            <div class="card-pile__card-details">
-                <h4>#{{ $card->id->value }} - {{ $card->kurzversion }}</h4>
-                <p>
-                    {{ $card->langversion }}
-                </p>
+            <li class="card-pile__card @if ($this->cardActionsVisible($card->id->value)) card-pile__card--show-actions @endif" wire:click="showCardActions('{{$card->id->value}}')">
+                <div class="card-pile__card-details">
+                    <h4>#{{ $card->id->value }} - {{ $card->kurzversion }}</h4>
+                    <p>
+                        {{ $card->langversion }}
+                    </p>
 
-                <h5>Voraussetzungen:</h5>
-                <ul>
-                    @if ($card->requirements->guthaben)<li>Guthaben: {{ $card->requirements->guthaben}}€</li>@endif
-                    @if ($card->requirements->zeitsteine)<li>Zeitsteine: {{ $card->requirements->zeitsteine}}</li>@endif
-                    @if ($card->requirements->bildungKompetenzsteine)<li>Bildung: {{ $card->requirements->bildungKompetenzsteine}}</li>@endif
-                    @if ($card->requirements->freizeitKompetenzsteine)<li>Freizeit: {{ $card->requirements->freizeitKompetenzsteine}}</li>@endif
-                </ul>
+                    <h5>Voraussetzungen:</h5>
+                    <ul>
+                        @if ($card->requirements->guthaben)<li>Guthaben: {{ $card->requirements->guthaben}}€</li>@endif
+                        @if ($card->requirements->zeitsteine)<li>Zeitsteine: {{ $card->requirements->zeitsteine}}</li>@endif
+                        @if ($card->requirements->bildungKompetenzsteine)<li>Bildung: {{ $card->requirements->bildungKompetenzsteine}}</li>@endif
+                        @if ($card->requirements->freizeitKompetenzsteine)<li>Freizeit: {{ $card->requirements->freizeitKompetenzsteine}}</li>@endif
+                    </ul>
 
-                <h5>Bringt dir:</h5>
-                <ul>
-                    @if ($card->resourceChanges->guthabenChange)<li>Guthaben: {{ $card->resourceChanges->guthabenChange}}€</li>@endif
-                    @if ($card->resourceChanges->zeitsteineChange)<li>Zeitstein: {{ $card->resourceChanges->zeitsteineChange}}</li>@endif
-                    @if ($card->resourceChanges->bildungKompetenzsteinChange)<li>Bildung: {{ $card->resourceChanges->bildungKompetenzsteinChange}}</li>@endif
-                    @if ($card->resourceChanges->freizeitKompetenzsteinChange)<li>Freizeit: {{ $card->resourceChanges->freizeitKompetenzsteinChange}}</li>@endif
-                    @if ($card->resourceChanges->newErwerbseinkommen)<li>Erwerbseinkommen: {{ $card->resourceChanges->newErwerbseinkommen}}€</li>@endif
-                    @if ($card->resourceChanges->erwerbseinkommenChangeInPercent)<li>Erwebseinkommen %: {{ $card->resourceChanges->erwerbseinkommenChangeInPercent}}%</li>@endif
-                </ul>
-            </div>
-
-            @if ($this->cardActionsVisible($card->id->value))
-                <div class="card-pile__card-actions">
-                    <button type="button" class="button button--type-outline-primary" wire:click="skipCard('{{$card->id->value}}')">Karte skippen</button>
-                    <button type="button" class="button button--type-primary" wire:click="activateCard('{{$card->id->value}}')">Karte spielen</button>
+                    <h5>Bringt dir:</h5>
+                    <ul>
+                        @if ($card->resourceChanges->guthabenChange)<li>Guthaben: {{ $card->resourceChanges->guthabenChange}}€</li>@endif
+                        @if ($card->resourceChanges->zeitsteineChange)<li>Zeitstein: {{ $card->resourceChanges->zeitsteineChange}}</li>@endif
+                        @if ($card->resourceChanges->bildungKompetenzsteinChange)<li>Bildung: {{ $card->resourceChanges->bildungKompetenzsteinChange}}</li>@endif
+                        @if ($card->resourceChanges->freizeitKompetenzsteinChange)<li>Freizeit: {{ $card->resourceChanges->freizeitKompetenzsteinChange}}</li>@endif
+                        @if ($card->resourceChanges->newErwerbseinkommen)<li>Erwerbseinkommen: {{ $card->resourceChanges->newErwerbseinkommen}}€</li>@endif
+                        @if ($card->resourceChanges->erwerbseinkommenChangeInPercent)<li>Erwebseinkommen %: {{ $card->resourceChanges->erwerbseinkommenChangeInPercent}}%</li>@endif
+                    </ul>
                 </div>
-            @endif
-        </li>
+
+                @if ($this->cardActionsVisible($card->id->value))
+                    <div class="card-pile__card-actions">
+                        <button type="button" class="button button--type-outline-primary" wire:click="skipCard('{{$card->id->value}}', '{{$card->pileId->value}}')">Karte skippen</button>
+                        <button type="button" class="button button--type-primary" wire:click="activateCard('{{$card->id->value}}', '{{$card->pileId->value}}')">Karte spielen</button>
+                    </div>
+                @endif
+            </li>
         @endif
     </ul>
 </div>
