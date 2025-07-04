886f272b056672e33cac478faefd53d44658dedb
Nicole <nicole.schulze@sandstorm.de>
2025-07-04 14:23:45 +0200

    feature: start with phasenwechsel

---
+++ app/src/CoreGameLogic/Feature/Spielzug/Event/LebenszielphaseWasChanged.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Domain\CoreGameLogic\Feature\Spielzug\Event;
+
+use Domain\CoreGameLogic\EventStore\GameEventInterface;
+use Domain\CoreGameLogic\Feature\Spielzug\Event\Behavior\ProvidesResourceChanges;
+use Domain\CoreGameLogic\PlayerId;
+use Domain\Definitions\Card\Dto\ResourceChanges;
+
+final readonly class LebenszielphaseWasChanged implements GameEventInterface, ProvidesResourceChanges
+{
+    public function __construct(
+        public PlayerId        $playerId,
+        public ResourceChanges $resourceChanges,
+        public int             $currentPhase,
+    )
+    {
+    }
+
+    public static function fromArray(array $values): GameEventInterface
+    {
+        return new self(
+            playerId: PlayerId::fromString($values['playerid']),
+            resourceChanges: ResourceChanges::fromArray($values['resourceChange']),
+            currentPhase: $values['currentPhase'],
+        );
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'playerId' => $this->playerId,
+            'resourceChanges' => $this->resourceChanges,
+            'currentPhase' => $this->currentPhase,
+        ];
+    }
+
+    public function getResourceChanges(PlayerId $playerId): ResourceChanges
+    {
+        if($playerId->equals($this->playerId)) {
+            return $this->resourceChanges;
+        }
+        return new ResourceChanges();
+    }
+
+    public function getPlayerId(): PlayerId
+    {
+        return $this->playerId;
+    }
+}
