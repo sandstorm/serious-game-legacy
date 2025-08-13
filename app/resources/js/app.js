d7f658ecd21e2738bf98484bc5b675fd3662f8a6
Michael <michael.berhorst@sandstorm-media.de>
2025-08-13 13:33:42 +0200

    TASK: Slide up to close player list
    Addresses: #328

--- app/resources/js/app.js
+++ app/resources/js/app.js
@@ -5,5 +5,9 @@ import.meta.glob('../views/**/*.css', { eager: true });
 
 import Alpine from 'alpinejs'
 import focus from '@alpinejs/focus'
+import playerList from './alpinejs/playerList'
 
 Alpine.plugin(focus)
+
+Alpine.data('playerList', playerList)
+Alpine.start()
