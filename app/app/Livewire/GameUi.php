sandstorm/serious-game-legacy

commit a73ba85c9d97cce38b08828c82d197b3d0411534

fix: show correct lebenszielphase in player-details
Closes: #317

app/resources/views/components/gameboard/phase-icon.blade.php
@@ -1,2 +1,5 @@
-<i @class(["icon-phase-" . $this->getPlayerPhase()]) aria-hidden="true"></i>
-<span class="sr-only">Phase {{ $this->getPlayerPhase() }}</span>
+@props([
+    '$lebenszielPhase' => null,
+])
+<i @class(["icon-phase-" . $lebenszielPhase]) aria-hidden="true"></i>
+<span class="sr-only">Phase {{ $lebenszielPhase }}</span>

app/resources/views/components/gameboard/playerList/player-list.blade.php
@@ -38,7 +38,7 @@
                 </ul>
 
                 <div class="player-list__player-phase">
-                    <x-gameboard.phase-icon />
+                    <x-gameboard.phase-icon :lebensziel-phase="$player->phaseDefinition->lebenszielPhaseId->value"/>
                 </div>
             </button>
 

commit c3885ab7fabfbdc50ce872c9b480b80d5607d4b9

fix: use current player as default for lebenszielPhaseIcon
This fixes a bug introduced in a73ba85c

Refs: #317

app/app/Livewire/GameUi.php
@@ -147,8 +147,8 @@ class GameUi extends Component
         return PlayerState::getPlayerColorClass($this->gameEvents, $this->myself);
     }
 
-    public function getPlayerPhase(): int
+    public function getPlayerPhase(PlayerId $playerId = null): int
     {
-        return PlayerState::getCurrentLebenszielphaseIdForPlayer($this->gameEvents, $this->myself)->value;
+        return PlayerState::getCurrentLebenszielphaseIdForPlayer($this->gameEvents, $playerId ?? $this->myself)->value;
     }
 }

app/resources/views/components/gameboard/phase-icon.blade.php
@@ -1,5 +1,5 @@
 @props([
-    '$lebenszielPhase' => null,
+    '$playerId' => null,
 ])
-<i @class(["icon-phase-" . $lebenszielPhase]) aria-hidden="true"></i>
-<span class="sr-only">Phase {{ $lebenszielPhase }}</span>
+<i @class(["icon-phase-" . $this->getPlayerPhase($playerId ?? null)]) aria-hidden="true"></i>
+<span class="sr-only">Phase {{ $this->getPlayerPhase($playerId ?? null) }}</span>

app/resources/views/components/gameboard/playerList/player-list.blade.php
@@ -38,7 +38,7 @@
                 </ul>
 
                 <div class="player-list__player-phase">
-                    <x-gameboard.phase-icon :lebensziel-phase="$player->phaseDefinition->lebenszielPhaseId->value"/>
+                    <x-gameboard.phase-icon :player-id="$player->playerId"/>
                 </div>
             </button>
 