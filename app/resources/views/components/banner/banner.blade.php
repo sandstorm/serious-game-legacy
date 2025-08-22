3c8422ed13fd3a8ed4e87ee88c5c6cd0efb7bdd0
Michael <michael.berhorst@sandstorm-media.de>
2025-08-22 12:25:33 +0200

    BUGFIX: Fixed banner
    * manually closing the banner now stops the timer

--- app/resources/views/components/banner/banner.blade.php
+++ app/resources/views/components/banner/banner.blade.php
@@ -1,40 +1,40 @@
 @use('\Domain\Definitions\Card\Dto\ResourceChanges')
 
-@if ($this->isBannerVisible)
-    <div
-        x-data="{ visible: @entangle('isBannerVisible'), message: @entangle('bannerMessage'), timer: null }"
-        @class([
-            "banner",
-            $this->getPlayerColorClass()
-        ])
-        x-init="
-            timer = setTimeout(() => $wire.closeBanner(), 10000);
-            $watch('message', () => {
-                // reset css animation on button
-                $refs.bannerCloseButton.style.animation = 'none';
-                $refs.bannerCloseButton.offsetHeight;
-                $refs.bannerCloseButton.style.animation = null;
-                // reset timer
-                clearTimeout(timer);
-                // set new timer
-                timer = setTimeout(() => $wire.closeBanner(), 10000)
-            })"
-    >
-        <div class="banner__content"
-            @if ($this->getBannerResourceChanges())
-                <div class="banner__resource-changes">
-                    <x-gameboard.resourceChanges.resource-changes style-class="horizontal" :resource-changes="$this->getBannerResourceChanges()" />
-                </div>
-            @endif
-
-            <div class="banner__body">
-                {{$this->bannerMessage}}
+<div
+    x-data="{ visible: @entangle('isBannerVisible'), message: @entangle('bannerMessage'), timer: null }"
+    @class([
+        "banner",
+        $this->getPlayerColorClass()
+    ])
+    x-cloak
+    x-show="visible"
+    x-init="
+        timer = setTimeout(() => $wire.closeBanner(), 10000);
+        $watch('message', () => {
+            // reset css animation on button
+            $refs.bannerCloseButton.style.animation = 'none';
+            $refs.bannerCloseButton.offsetHeight;
+            $refs.bannerCloseButton.style.animation = null;
+            // reset timer
+            clearTimeout(timer);
+            // set new timer
+            timer = setTimeout(() => $wire.closeBanner(), 10000)
+        })"
+>
+    <div class="banner__content"
+        @if ($this->getBannerResourceChanges())
+            <div class="banner__resource-changes">
+                <x-gameboard.resourceChanges.resource-changes style-class="horizontal" :resource-changes="$this->getBannerResourceChanges()" />
             </div>
+        @endif
 
-            <button x-ref="bannerCloseButton" class="banner__close button button--type-text" wire:click="closeBanner()">
-                <i class="icon-close" aria-hidden="true"></i>
-                <span class="sr-only">Banner schließen</span>
-            </button>
+        <div class="banner__body">
+            {{$this->bannerMessage}}
         </div>
+
+        <button x-ref="bannerCloseButton" class="banner__close button button--type-text" x-on:click="clearTimeout(timer); $wire.closeBanner()">
+            <i class="icon-close" aria-hidden="true"></i>
+            <span class="sr-only">Banner schließen</span>
+        </button>
     </div>
-@endif
+</div>
