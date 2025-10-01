93b40e37dfb3d3132f5b0b63b6d966321190cb63
Martin Hauke <martin.hauke@sandstorm.de>
2025-10-01 15:46:32 +0200

    fixup! feat: Admin UI

--- app/app/Models/Course.php
+++ app/app/Models/Course.php
@@ -8,6 +8,17 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Course extends Model
 {
     /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'teacher',
+    ];
+
+
+    /**
      * @return BelongsTo<User, $this>
      */
     public function teacher(): BelongsTo
