17ba15fe539def3fa0c75a957897b6370eb83aeb
Michael <michael.berhorst@sandstorm-media.de>
2025-10-07 13:54:53 +0200

    TASK: Performance Profiler
    Addresses: #457

--- deployment/config-files/auto_prepend_file.php
+++ deployment/config-files/auto_prepend_file.php
@@ -5,7 +5,8 @@
 // profiling.
 // !!!!!!!!!!!!! DO NOT DELETE THIS FILE !!!!!!!!!!!!!!!!!!!!
 
-function startExcimer() {
+function startExcimer()
+{
     static $excimer;
     if (!class_exists(\ExcimerProfiler::class)) {
         // excimer.so profiling extension not loaded.
@@ -13,15 +14,46 @@ function startExcimer() {
     }
 
     $excimer = new ExcimerProfiler();
-    $excimer->setPeriod( 0.001 ); // 1ms
-    $excimer->setEventType( EXCIMER_REAL ); // OR: EXCIMER_CPU, but does not work on FreeBSD.
+    $excimer->setPeriod(1);
+
+    $excimer->setEventType(EXCIMER_REAL); // OR: EXCIMER_CPU, but does not work on FreeBSD.
     $excimer->start();
-    register_shutdown_function( function () use ( $excimer ) {
+
+    register_shutdown_function(function () use ($excimer) {
         $excimer->stop();
         $data = $excimer->getLog()->formatCollapsed();
-        file_put_contents('/tracing/_traces/' . getmypid(), $data, FILE_APPEND);
-    } );
+
+        # return early if no traces collected
+        if($data === ""){
+            return;
+        }
+
+        $url = getenv("PROFILING_VECTOR_URL", "0.0.0.0:8234");
+        $username = getenv("PROFILING_VECTOR_USER", "user"); // vector user
+        $password = getenv("PROFILING_VECTOR_PASSWORD", "password"); // vector password
+
+        if ($_SERVER && isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_METHOD']) {
+            $request_path = $_SERVER['REQUEST_URI'];
+            $http_method = $_SERVER['REQUEST_METHOD'];
+        }
+
+        $ch = curl_init($url);
+        // Set cURL options
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+            'traces' => $data,
+            // set to null so default of database is used
+            'request_path' => $request_path ?? null,
+            'http_method' => $http_method ?? null,
+        ]));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+        curl_exec($ch);
+
+        curl_close($ch);
+    });
 }
 
-// HINT: to start PHP continuous profiling, comment-in the following line.
-// startExcimer();
+// HINT: to stop PHP continuous profiling, comment-in the following line.
+startExcimer();
