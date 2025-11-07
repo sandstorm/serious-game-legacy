cec13fe51a5b0995dac0cece9aa37d454d3e51a5
Martin Hauke <martin.hauke@sandstorm.de>
2025-11-07 12:51:33 +0100

    fix: remove dev tools from game
    The final product should not have the dev-log and player-links visible.
    
    Closes: #551

--- app/app/Livewire/Traits/HasLog.php
+++ app/app/Livewire/Traits/HasLog.php
@@ -12,36 +12,6 @@ use Domain\CoreGameLogic\Feature\Spielzug\State\PlayerState;
 
 trait HasLog
 {
-    public bool $isLogVisible = false;
-
-    public function showLog(): void
-    {
-        $this->isLogVisible = true;
-    }
-
-    public function closeLog(): void
-    {
-        $this->isLogVisible = false;
-    }
-
-    /**
-     * TODO we will probably move this to the events and let them implement an interface (e.g. `logable`) that provides
-     * a uniform structure (e.g. playerId (if any), eventName, changes/effects/consequences, ... )
-     * @return array<array<mixed>>
-     */
-    private function getPrettyEvents(): array
-    {
-        return array_map(function (GameEventInterface $event) {
-            $items = [];
-            $classPath = explode('\\', $event::class);
-            $items['eventClass'] = end($classPath);
-            foreach ($event->jsonSerialize() as $key => $item) {
-                $items[$key] = json_encode($item);
-            }
-            return $items;
-        }, $this->getGameEvents()->events);
-    }
-
     /**
      * @return EventLogEntry[]
      */
