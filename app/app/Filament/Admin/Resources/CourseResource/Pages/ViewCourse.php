cd9ad75ab97219a21f330bf1992fbec568ebf996
Martin Hauke <martin.hauke@sandstorm.de>
2025-11-07 10:16:30 +0100

    fix: create random games with correct player count
    Creating games now creates random groups of two to four players. It is
    also possible to create a new set of games.
    
    Closes: #546
    Closes: #548
    
    # Conflicts:
    #	app/app/Filament/Admin/Resources/CourseResource/Pages/ViewCourse.php

--- app/app/Filament/Admin/Resources/CourseResource/Pages/ViewCourse.php
+++ app/app/Filament/Admin/Resources/CourseResource/Pages/ViewCourse.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Filament\Admin\Resources\CourseResource\Pages;
@@ -15,12 +16,72 @@ use Domain\CoreGameLogic\PlayerId;
 use Filament\Actions\Action;
 use Filament\Actions\ImportAction;
 use Filament\Facades\Filament;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Support\Str;
 
 class ViewCourse extends ViewRecord
 {
     protected static string $resource = CourseResource::class;
+
+    /**
+     * @param Collection<int, Player> $playersCollection
+     * @return array<array<Player>>
+     */
+    private static function createRandomGameGroups(Collection $playersCollection): array
+    {
+        $allPlayers = [];
+
+        foreach ($playersCollection as $player) {
+            $allPlayers[] = $player;
+        }
+
+        // We need at least two players
+        if (count($allPlayers) < 2) {
+            return [];
+        }
+
+        // we want random groups, so we shuffle all players before slicing the array into groups
+        shuffle($allPlayers);
+
+        $gameGroups = [];
+
+        /**
+         * WHY:
+         * We need at least 2 players in a game. If we fill all games with four players we may end up
+         * with one group that only has 1 player. That is the case when playerCount % 4 === 1. In that
+         * case we can simply create one group with 2 players and one group with 3 players. The remaining
+         * players can be put in full groups.
+         */
+        if (count($allPlayers) > 4 && count($allPlayers) % 4 === 1) {
+            $gameGroups[] = array_slice($allPlayers, 0, 3);
+            $gameGroups[] = array_slice($allPlayers, 3, 2);
+            $allPlayers = array_slice($allPlayers, 5);
+        }
+
+        /**
+         * Fill the remaining games with up to 4 players (unless there are less than 4 left)
+         */
+        $playersPerGame = 4;
+        $numberOfGames = (int) ceil(count($allPlayers) / $playersPerGame);
+        for ($i = 0; $i < $numberOfGames; $i++) {
+            $gameGroups[] = array_slice($allPlayers, $i * $playersPerGame, $playersPerGame);
+        }
+
+        return $gameGroups;
+    }
+
+    /**
+     * Proxy method to test the private function `createRandomGameGroups`.
+     * Calling `...ForTesting` functions outside of test code will be caught by phpstan.
+     * @param Collection<int, Player> $playersCollection
+     * @return array<array<Player>>
+     */
+    public static function createRandomGameGroupsForTesting(Collection $playersCollection): array
+    {
+        return self::createRandomGameGroups($playersCollection);
+    }
 
     protected function getHeaderActions(): array
     {
@@ -34,35 +95,31 @@ class ViewCourse extends ViewRecord
                 ]),
             Action::make('Spiele erstellen')
                 ->action(function (ForCoreGameLogic $coreGameLogic) {
+
                     /** @var Course $course */
                     $course = $this->record;
-
-                    $allPlayers = $course->players;
-                    $players = [];
-                    // TODO players who created their own game are not included at this point
-                    foreach ($allPlayers as $player) {
-                        // check if player is already in a game for this course
-                        /** @phpstan-ignore staticMethod.dynamicCall, staticMethod.dynamicCall */
-                        if ($player->games()->where('course_id', $course->id)->count() === 0) {
-                            $players[] = $player;
-                        }
-                    }
-
                     // current user is the creator of the games
                     $panel = Filament::getCurrentPanel();
                     $user = $panel?->auth()->user();
 
-                    // TODO add min players check (2)
-                    $playersPerGame = 4;
-                    $numberOfGames = (int) ceil(count($players) / $playersPerGame);
-                    for ($i = 0; $i < $numberOfGames; $i++) {
+                    $gameGroups = self::createRandomGameGroups($course->players);
+
+                    if (count($gameGroups) === 0) {
+                        Notification::make()
+                            ->title('Es werden mindestens zwei Spielende benötigt, um Spiele zu erstellen')
+                            ->warning()
+                            ->send();
+                        return;
+                    }
+
+                    foreach ($gameGroups as $group) {
                         $game = new Game();
                         /** @phpstan-ignore assign.propertyType */
                         $game->id = Str::ulid();
                         $game->course()->associate($course);
                         $game->creator()->associate($user); // no creator
                         $game->save();
-                        $game->players()->attach(array_slice($players, $i * $playersPerGame, $playersPerGame));
+                        $game->players()->attach($group);
                         $coreGameLogic->handle(GameId::fromString((string) $game->id), StartPreGame::create(
                             numberOfPlayers: count($game->players)
                         )->withFixedPlayerIds(
