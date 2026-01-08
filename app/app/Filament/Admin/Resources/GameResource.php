0c1127a14ef1f30a219565cfbc0c7634b73b6925
Martin Hauke <martin.hauke@sandstorm.de>
2026-01-08 13:08:00 +0100

    fix: use SoSciSurveyIds in json logs

--- app/app/Filament/Admin/Resources/GameResource.php
+++ app/app/Filament/Admin/Resources/GameResource.php
@@ -6,7 +6,10 @@ namespace App\Filament\Admin\Resources;
 use App\Filament\Admin\Resources\GameResource\Pages;
 use App\Infolists\Components\PlayerTable;
 use App\Models\Game;
+use App\Models\Player;
 use Domain\CoreGameLogic\DrivingPorts\ForCoreGameLogic;
+use Domain\CoreGameLogic\EventStore\GameEvents;
+use Domain\CoreGameLogic\Feature\Initialization\Event\GameWasStarted;
 use Domain\CoreGameLogic\GameId;
 use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
@@ -29,6 +32,39 @@ class GameResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    /**
+     * We want to have the SoSciSurvey Ids in the JSON log, not out internal Ids. This function
+     * returns a mapping from our internal Ids to the SoSciSurvey Id of the player.
+     *
+     * We use the User->email field for the SoSciSurvey Id
+     * External Players don't have a SoSciSurvey Id. Their Ids will not be mapped.
+     * @param GameEvents $gameEvents
+     * @return array<string,string>
+     */
+    private static function getPlayerIdToSoSciSurveyIdMappings(GameEvents $gameEvents): array
+    {
+        $gameWasStartedEvent = $gameEvents->findLastOrNull(GameWasStarted::class);
+        if ($gameWasStartedEvent === null) {
+            return  [];
+        }
+        $mappings = [];
+        foreach ($gameWasStartedEvent->playerOrdering as $id) {
+            $soSciSurveyId = Player::find($id->value)?->email;
+            if ($soSciSurveyId === null) {
+                continue;
+            }
+            $mappings[$id->value] = $soSciSurveyId ;
+        }
+        return $mappings;
+    }
+
+    /**
+     * Returns the game event logs as Json.
+     * @param Game $record
+     * @param ForCoreGameLogic $coreGameLogic
+     * @return string
+     * @throws \JsonException
+     */
     protected static function getLogs(Game $record, ForCoreGameLogic $coreGameLogic): string
     {
         $gameEvents = $coreGameLogic->getGameEvents(GameId::fromString($record['id']));
@@ -41,7 +77,12 @@ class GameResource extends Resource
                 "data" => $gameEvent,
             ];
         }
-        return json_encode($list, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_THROW_ON_ERROR);
+        $playerIdsToSoSciSurveyIdsMapping = self::getPlayerIdToSoSciSurveyIdMappings($gameEvents);
+        return str_replace( // replace internal ids with SoSciSurveyIds
+            array_keys($playerIdsToSoSciSurveyIdsMapping),
+            array_values($playerIdsToSoSciSurveyIdsMapping),
+            json_encode($list, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_THROW_ON_ERROR)
+        );
     }
 
     public static function table(Table $table): Table
